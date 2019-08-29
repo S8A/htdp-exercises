73266faf34c234611563c11b0b5e9b4c5d10dd8c
S8A <samuelochoap@gmail.com>
2019-08-29 10:55:40 -0400

    Fixed signature of exercise 252 (sec. 15.1, part 3)

--- ex252.rkt
+++ ex252.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex252) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
-; [X X -> X] X [List-of X] -> [List-of X]
+; [X Y] [X Y -> Y] Y [List-of X] -> [List-of Y]
 (define (fold2 f n l)
   (cond
     [(empty? l) n]
