5b2890bb16483f7afd770abe61282274fe226ebf
S8A <samuelochoap@gmail.com>
2019-08-29 21:05:52 -0400

    Fixed signatures, exercises from sec. 15.2, part 3

--- ex254.rkt
+++ ex254.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex254) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
-; [List-of X] [X X -> Boolean] -> [List-of X]
+; [X] [List-of X] [X X -> Boolean] -> [List-of X]
 ; Sorts the given list using comparator c
 (define (sort-l l c)
   (cond
--- ex255.rkt
+++ ex255.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex255) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
-; [List-of X] [X -> X] -> [List-of X]
+; [X] [List-of X] [X -> X] -> [List-of X]
 ; Applies function f to each item from the list.
 (define (map-l l f)
   (cond
@@ -35,7 +35,7 @@
 
 
 
-; [List-of X] [X -> Y] -> [List-of Y]
+; [X Y] [List-of X] [X -> Y] -> [List-of Y]
 (define (map1 k g)
   (map-l k g))
 	
